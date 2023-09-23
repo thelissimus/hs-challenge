8f47bb2fc769fbebae2d77c5fa2a4ccf1b868b48
thelissimus <thelissimus@tuta.io>
2023-09-23 22:26:37 +0500

    setup: deps

--- project.clj
+++ project.clj
@@ -1,7 +1,10 @@
 (defproject challenge "0.1.0-SNAPSHOT"
   :description "Health Samurai Challenge"
   :license {:name "BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [http-kit/http-kit "2.7.0"]
+                 [com.github.seancorfield/next.jdbc "1.3.883"]
+                 [org.postgresql/postgresql "42.6.0"]]
   :main ^:skip-aot challenge.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
