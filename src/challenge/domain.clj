c389b8a4fa8532878268a4573416dbd7dcbcc1d4
thelissimus <thelissimus@tuta.io>
2023-10-04 04:17:03 +0500

    refactor: domain

--- src/challenge/domain.clj
+++ src/challenge/domain.clj
@@ -1,14 +1,16 @@
 (ns challenge.domain
   (:require [clojure.spec.alpha :as s]))
 
-(s/def ::first-name #(and (string? %) (>= 255 (count %))))
-(s/def ::middle-name #(and (string? %) (>= 255 (count %))))
-(s/def ::last-name #(and (string? %) (>= 255 (count %))))
+(s/def ::first-name (s/and string? #(>= 255 (count %))))
+(s/def ::middle-name (s/and string? #(>= 255 (count %))))
+(s/def ::last-name (s/and string? #(>= 255 (count %))))
 (s/def ::sex #{"male" "female"})
 (s/def ::birth-date #(instance? java.time.LocalDateTime %))
 (s/def ::address string?)
 (s/def ::insurance string?)
 (s/def ::patient
   (s/keys :req-un [::first-name ::middle-name ::last-name ::sex ::birth-date ::address ::insurance]))
+(s/def ::patient-partial
+  (s/keys :opt-un [::first-name ::middle-name ::last-name ::sex ::birth-date ::address ::insurance]))
 
-(defrecord Patient [first-name middle-name last-name sex birth-date address insurance])
+(defrecord Patient [id first-name middle-name last-name sex birth-date address insurance])
