1ee49b1bd31bff99d3d8ea18ad82c64abe980a69
thelissimus <thelissimus@tuta.io>
2023-10-08 15:33:58 +0500

    fix: domain

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
                  [org.slf4j/slf4j-api "2.0.9"]
                  [org.slf4j/slf4j-simple "2.0.9"]
                  [com.taoensso/timbre "6.2.2"]
+                 [clojure.java-time "1.3.0"]
                  [cheshire "5.12.0"]]
   :main ^:skip-aot challenge.core
   :target-path "target/%s"
--- src/challenge/domain.clj
+++ src/challenge/domain.clj
@@ -1,16 +1,19 @@
 (ns challenge.domain
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [java-time.api :as time])
+  (:import (java.time.format DateTimeParseException)))
 
-(s/def ::first-name (s/and string? #(>= 255 (count %))))
-(s/def ::middle-name (s/and string? #(>= 255 (count %))))
-(s/def ::last-name (s/and string? #(>= 255 (count %))))
+(s/def ::first_name (s/and string? #(>= 255 (count %))))
+(s/def ::middle_name (s/and string? #(>= 255 (count %))))
+(s/def ::last_name (s/and string? #(>= 255 (count %))))
 (s/def ::sex #{"male" "female"})
-(s/def ::birth-date #(instance? java.time.LocalDateTime %))
+(s/def ::birth_date (s/conformer #(try (time/local-date-time %)
+                                       (catch DateTimeParseException _ ::s/invalid))))
 (s/def ::address string?)
 (s/def ::insurance string?)
 (s/def ::patient
-  (s/keys :req-un [::first-name ::middle-name ::last-name ::sex ::birth-date ::address ::insurance]))
+  (s/keys :req-un [::first_name ::middle_name ::last_name ::sex ::birth_date ::address ::insurance]))
 (s/def ::patient-partial
-  (s/keys :opt-un [::first-name ::middle-name ::last-name ::sex ::birth-date ::address ::insurance]))
+  (s/keys :opt-un [::first_name ::middle_name ::last_name ::sex ::birth_date ::address ::insurance]))
 
-(defrecord Patient [id first-name middle-name last-name sex birth-date address insurance])
+(defrecord Patient [first_name middle_name last_name sex birth_date address insurance])
