a65d046dee1f964b04ff4a691fa85d2e0dd4904d
thelissimus <thelissimus@tuta.io>
2023-10-08 16:43:16 +0500

    add: conform-let and conform-let*

---
+++ src/challenge/lib.clj
@@ -0,0 +1,16 @@
+(ns challenge.lib)
+
+(defmacro conform-let
+  {:clj-kondo/lint-as 'clojure.core/let}
+  [[sym expr] & body]
+  `(let [~sym ~expr]
+     (when-not (s/invalid? ~sym)
+       (do ~@body))))
+
+(defmacro conform-let*
+  {:clj-kondo/lint-as 'clojure.core/let}
+  [bindings & body]
+  (if (seq bindings)
+    `(conform-let [~(first bindings) ~(second bindings)]
+                  (conform-let* ~(vec (drop 2 bindings)) ~@body))
+    `(do ~@body)))
