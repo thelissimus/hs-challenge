3153344d2fbdc880092e28222744b5e77a0f3460
thelissimus <thelissimus@tuta.io>
2023-10-17 08:37:41 +0500

    setup: routes

--- src/challenge/frontend/core.cljs
+++ src/challenge/frontend/core.cljs
@@ -1,10 +1,43 @@
 (ns challenge.frontend.core
   (:require [reagent.core :as r]
-            [reagent.dom :as rd]))
+            [reagent.dom :as rd]
+            [reitit.frontend :as rf]
+            [reitit.frontend.easy :as rfe]
+            [reitit.coercion.spec :as rcs]
+            [challenge.frontend.view :as view]))
 
-(defn app []
+(defonce match (r/atom nil))
+
+(defn current-page []
   [:div
-   [:h1 "Hello, World"]])
+   [:ul
+    [:li [:a {:href (rfe/href ::home-page)} "Home page"]]
+    [:li [:a {:href (rfe/href ::patients-list)} "Patients list"]]]
+   (when @match
+     (let [view (:view (:data @match))]
+       [view @match]))])
+
+(def routes
+  [["/"
+    {:name ::home-page
+     :view view/home-page}]
+
+   ["/patients"
+    {:name ::patients-list
+     :view view/patients-list}]
+
+   ["/patients/:id"
+    {:name ::patient-info
+     :view view/patient-info
+     :parameters {:path {:id int?}}}]
+
+   ["/patients/:id/edit"
+    {:name ::patient-edit
+     :view view/patient-edit
+     :parameters {:path {:id int?}}}]])
 
 (defn init []
-  (rd/render [app] (.-body js/document)))
+  (rfe/start! (rf/router routes {:data {:coercion rcs/coercion}})
+              (fn [m] (reset! match m))
+              {:use-fragment true})
+  (rd/render [current-page] (.-body js/document)))
---
+++ src/challenge/frontend/view.cljs
@@ -0,0 +1,13 @@
+(ns challenge.frontend.view)
+
+(defn home-page []
+  [:h2 "Home"])
+
+(defn patients-list []
+  [:h2 "Patients list"])
+
+(defn patient-info []
+  [:h2 "Patient info"])
+
+(defn patient-edit []
+  [:h2 "Patient edit"])
