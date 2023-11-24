ff4fa4321502b829e1a79597338d092d263482c0
thelissimus <thelissimus@tuta.io>
2023-11-25 03:56:01 +0500

    add: test server base

--- test/challenge/backend/core_test.clj
+++ test/challenge/backend/core_test.clj
@@ -4,12 +4,15 @@
    [clojure.test :refer [deftest is testing run-tests use-fixtures]]
    [clojure.spec.alpha :as s]
    [clojure.spec.gen.alpha :as gen]
+   [org.httpkit.server :refer [run-server]]
    [next.jdbc :refer [execute!]]
    [next.jdbc.connection :refer [->pool]]
+   [challenge.backend.server :refer [app]]
    [challenge.backend.domain :as domain])
   (:import (com.zaxxer.hikari HikariDataSource)))
 
 (def ds (atom nil))
+(def server (atom nil))
 
 (deftest patients-get-all
   (testing "Empty result"
@@ -30,11 +33,17 @@
   (tests)
   (.close @ds))
 
+(defn setup-server [tests]
+  (reset! server (run-server (app ds) {:port 8080}))
+  (tests)
+  (@server))
+
 (defn reset-database [tests]
   (execute! @ds [(slurp (io/resource "schema.sql"))])
   (tests)
   (execute! @ds [(slurp (io/resource "down.sql"))]))
 
+(use-fixtures :once setup-server)
 (use-fixtures :once setup-database)
 (use-fixtures :each reset-database)
 
