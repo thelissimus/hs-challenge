9664ac2862e9854497a7fc4c4ec7d11d1cfdd6e8
thelissimus <thelissimus@tuta.io>
2023-11-27 12:50:39 +0500

    add: GET /patients with populated db

--- test/challenge/backend/core_test.clj
+++ test/challenge/backend/core_test.clj
@@ -8,24 +8,24 @@
    [org.httpkit.server :refer [run-server]]
    [org.httpkit.client :refer [request]]
    [next.jdbc :refer [execute!]]
+   [next.jdbc.sql :refer [insert-multi!]]
    [next.jdbc.connection :refer [->pool]]
    [challenge.backend.server :refer [app]]
    [challenge.backend.domain :as domain])
   (:import (com.zaxxer.hikari HikariDataSource)))
 
+;;; constants
+
 (def ds (atom nil))
 (def port 8080)
 (def url (format "http://localhost:%d" port))
 
-(deftest patients-get-all
-  (testing "Empty result"
-    (is (= (json/parse-string-strict (:body @(request {:url (str url "/patients") :method :get})) true)
-           {:data [] :count 0})))
+;;; utils
 
-  (testing "Non-empty result"
-    (let [data (map (fn [_] (gen/generate (s/gen ::domain/patient))) (range 10))]
-      (is (= {:data data :count (count data)}
-             {:data data :count (count data)})))))
+(defn gen-patients [n]
+  (map (fn [_] (gen/generate (s/gen ::domain/patient))) (range n)))
+
+;;; fixtures
 
 (defn setup-server [tests]
   (let [datasource (->pool HikariDataSource
@@ -46,5 +46,21 @@
 
 (use-fixtures :once setup-server)
 (use-fixtures :each reset-database)
+
+;;; tests
+
+(deftest patients-get-all
+  (testing "Empty result"
+    (is (= (json/parse-string-strict (:body @(request {:url (str url "/patients") :method :get})) true)
+           {:data [] :count 0})))
+
+  (testing "Non-empty result"
+    (let [patients (gen-patients 5)
+          entities (map #(s/conform ::domain/patient %) patients)
+          enumerated (map-indexed (fn [i a] (merge a {:id (inc i)})) patients)]
+      (insert-multi! @ds :patients (-> entities first keys vec) (vec (map vals entities)))
+
+      (is (= (json/parse-string-strict (:body @(request {:url (str url "/patients") :method :get})) true)
+             {:data enumerated :count (count enumerated)})))))
 
 (run-tests)
